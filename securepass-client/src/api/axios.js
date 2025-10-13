f9e7017e8829895b34d832c539f894a46d170811
Shisham Chudal <shishamchudal61@gmail.com>
2025-10-13 15:57:40 -0700

    Updated server url

--- securepass-client/src/api/axios.js
+++ securepass-client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // backend URL
+  baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:4000/api",
   withCredentials: true,
 });
 
